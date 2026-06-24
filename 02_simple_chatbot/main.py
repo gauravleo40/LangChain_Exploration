72be882a040baf5fbebc742c7306872224aa27ba
Gaurav Singh <gauravleo@gmail.com>
2026-06-24 06:18:43 +0530

    removed previous comments

--- 02_simple_chatbot/main.py
+++ 02_simple_chatbot/main.py
@@ -10,12 +10,6 @@ from chain import chat
 
 st.title("Hospital Chatbot : Consult your doctor")
 st.divider()
-# domain = st.text_input("Enter the domain of your start-up", placeholder="E.g. Restaurant, Fitness, etc.")
-# begin_chat = st.button("begin_chat")
-# st.header(f"Domain Provided : {domain}")
-# st.divider()
-
-# response = chain.invoke({'domain':domain})
 
 st.header("Chatbot is ready. Type 'q' / 'exit' / 'quit' to exit the Chatbot\n")
 
@@ -40,16 +34,3 @@ for i in st.session_state.messages :
         st.chat_message('user').write(i['content'])
     else:
         st.chat_message('assistant').write(i['content'])
-
-# while True:
-#     user_input = st.text_input("Ask your query: ")
-#     if user_input.lower() in ["exit", "quit", "q"]:
-#         st.write(" ==== Exiting the Chatbot. Goodbye! ==== ")
-#         break
-#     else:
-#         doc_response = chat(user_input)
-#         st.write(doc_response)
-
-
-
-# st.markdown('\n'.join([f"- {i.business_name}" for i in response.allinfo]))
